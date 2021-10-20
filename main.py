14d1bf2599a6e19cfb40bcc64d3f029c6fd7733e
lucas.olivares <47455265+swavyfeel@users.noreply.github.com>
2021-10-20 15:56:51 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
-import pandas
-import
+import pandas as pd
+import numpy as np
+#from keras import Sequential
+#from keras.layers import Dense
 
-data = pandas.read_excel("Datos_PrActica_1_BPNN.xls", index_col=None)
+data = pd.read_excel('datosBPNN.xls')
+data['ACCIDENTE'] = pd.factorize(data['ACCIDENTE'])[0]
+X = np.array(data.iloc[:, 1:13])
+y = np.array(data.iloc[:, 13])
+print(y)
 
+#model = Sequential()
+#model.add(Dense(12, input_dim=11, activation='sigmoid'))
+#model.add(Dense(1, activation='sigmoid'))
+
+#model.compile(loss='mean_squared_error', optimizer='SGD', metrics=['loss', 'accuracy'])
+
+#model.fit(X, y, epochs=1500)
